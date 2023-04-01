45210e240442cc6a8bdb599ef6338e43c0d0c141
kmounish <73723664+kmounish@users.noreply.github.com>
2023-04-01 11:17:43 -0400

    Add files via upload

---
+++ importpenai.py
@@ -0,0 +1,33 @@
+import openai
+
+openai.api_key = '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'
+model_id = 'gpt-3.5-turbo'
+
+
+def ChatGPT_conversation(conversation):
+    response = openai.ChatCompletion.create(
+        model=model_id,
+        messages=conversation
+    )
+
+    conversation.append(
+        {'role': response.choices[0].message.role, 'content': response.choices[0].message.content})
+    return conversation
+
+
+conversation = []
+conversation.append({'role': 'system', 'content': 'Test'})
+conversation = ChatGPT_conversation(conversation)
+print('{0}: {1}\n'.format(
+    conversation[-1]['role'].strip(), conversation[-1]['content'].strip()))
+
+while True:
+    prompt = input('You: ')
+    response = openai.Moderation.create(
+        input=prompt
+    )
+    output = response["results"][0]
+    print(output)
+    # conversation.append({'role': 'user', 'content': prompt})
+    # conversation = ChatGPT_conversation(conversation)
+    # print('{0}: {1}\n'.format(conversation[-1]['role'].strip(), conversation[-1]['content'].strip()))
