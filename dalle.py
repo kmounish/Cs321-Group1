3ae8a191e490d8d3015da8dc1b797635c753cc58
Doug Jennings <doug.hj163@gmail.com>
2023-04-19 18:15:18 -0400

    Revert "rolling back key security measure"
    This reverts commit c07e425cda9bc6a216f936f75ae747b88199fc9d.

--- dalle.py
+++ dalle.py
@@ -2,7 +2,7 @@ import requests
 import os
 
 
-API_KEY = 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 #os.getenv("DALLE_API_KEY")
+API_KEY = os.getenv("DALLE_API_KEY")
 
 
 async def send_request(user_msg):
